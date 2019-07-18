85239782557a1999d2356f0c981c12608d3d906f
unknown <bbvardal@gmail.com>
2019-07-18 16:02:18 +0300

    small changes

--- Degiskenler/main.go
+++ Degiskenler/main.go
@@ -9,10 +9,12 @@ func main() {
 	var isim string = "Bülent"
 	var yas int32 = 42
 
+	ad, soyad := "Ahmet", "Yakar"
+
 	const havaSicak = true
 	takimAdi := "Beşiktaş"
 
-	fmt.Println(isim, yas, takimAdi)
+	fmt.Println(isim, yas, takimAdi, ad, soyad)
 	fmt.Printf("Yas Değişkenin Tip : %T \n", yas)
 	fmt.Printf("havaSicak Değişkenin Tip : %T \n", havaSicak)
 	fmt.Printf("takimAdi Değişkenin Tip : %T \n", takimAdi)
