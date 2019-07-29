462f6f35258a869bc71005ffa8b217eedacbd939
unknown <bbvardal@gmail.com>
2019-07-29 16:33:32 +0300

    interfece example improved

--- interfaceGo/main.go
+++ interfaceGo/main.go
@@ -2,36 +2,42 @@ package main
 
 import "fmt"
 
-
-type alanhesap interface{
-	alan() int
+type alanhesap interface {
+	alan() (int, string)
 }
 
-type dikdortgen struct{
+type dikdortgen struct {
 	aKenari int
 	bKenari int
 }
-type kare struct{
+type kare struct {
 	kenar int
 }
 
-func (d dikdortgen) alan() int{
-	return d.aKenari*d.bKenari
+func (d dikdortgen) alan() (int, string) {
+	return d.aKenari * d.bKenari, "Dikdorgen"
 }
 
-func (k kare) alan() int{
-	return k.kenar*k.kenar
+func (k kare) alan() (int, string) {
+	return k.kenar * k.kenar, "Kare"
 }
 
-func alanBul(a alanhesap){
-	fmt.Print(a.alan())
+func alanHsp(a alanhesap) {
+	fmt.Println(a.alan())
 }
 
-func main(){
-	dg:=dikdortgen{10,20}
-	kr:=kare{10}
+func alanBul(a ...alanhesap) {
+	for _, b := range a {
+		al, tip := b.alan()
+		fmt.Println(tip, " alanı =", al)
+	}
 
-	alanBul(dg)
-	alanBul(kr)
+}
 
+func main() {
+
+	alanHsp(dikdortgen{10, 20})
+	alanHsp(kare{10})
+
+	alanBul(kare{30}, dikdortgen{3, 4})
 }
